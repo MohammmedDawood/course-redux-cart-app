181399ce5341816e0a409a225f5496fa93b1f1ec
Mohammed Dawood <mahmedtony73@gmail.com>
2022-09-22 01:59:31 +0200

    Finalizing the Fetching Logic

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -56,7 +56,10 @@ export const sendCartData = (cart) => {
         "https://react-http-c5e4d-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
         {
           method: "PUT",
-          body: JSON.stringify(cart),
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
         }
       );
 
