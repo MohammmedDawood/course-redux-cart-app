f2a83510919249e0d9c15a78b7fedbfcba3a233d
Mohammed Dawood <mahmedtony73@gmail.com>
2022-09-22 00:15:34 +0200

    add cart slice reduecer

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -21,5 +21,20 @@ const cartSlice = createSlice({
         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
+    removeItem(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
   },
 });
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-// import cartSlice from "./cart-slice";, cart: cartSlice.reducer
 import uiSlice from "./ui-slice";
+import cartSlice from "./cart-slice";
 
 const store = configureStore({
-  reducer: { ui: uiSlice.reducer },
+  reducer: { ui: uiSlice.reducer, cart: cartSlice.reducer },
 });
 
 export default store;
